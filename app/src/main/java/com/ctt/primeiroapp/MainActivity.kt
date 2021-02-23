38d233c3f03bc178f29da812b18992a9ec779336
Pri Nitta <egashira.sayuri@gmail.com>
2021-02-22 22:05:08 -0300

    first commit :]

---
+++ app/src/main/java/com/ctt/primeiroapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ctt.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //codigo vem aqui!
+    }
+}
