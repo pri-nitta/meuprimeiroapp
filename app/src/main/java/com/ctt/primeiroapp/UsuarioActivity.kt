8391d95915da87a03e6a3c3bccd2be5617b653ca
Pri Nitta <egashira.sayuri@gmail.com>
2021-02-25 17:45:59 -0300

    Button creation and xml edition.

---
+++ app/src/main/java/com/ctt/primeiroapp/UsuarioActivity.kt
@@ -0,0 +1,11 @@
+package com.ctt.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_usuario)
+    }
+}
