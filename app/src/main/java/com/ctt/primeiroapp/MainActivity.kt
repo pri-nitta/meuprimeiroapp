19b73c1b7a00fa0be97716753338cffe75b533db
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-01 17:19:56 -0300

    New Activity and Intent creation

--- app/src/main/java/com/ctt/primeiroapp/MainActivity.kt
+++ app/src/main/java/com/ctt/primeiroapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ctt.primeiroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,41 +9,49 @@ import android.widget.EditText
 import android.widget.Toast
 import com.ctt.primeiroapp.model.Usuario
 import kotlinx.android.synthetic.main.activity_main.*
-
+private const val CICLO_VIDA = "CICLOVIDA"
 class MainActivity : AppCompatActivity() {
-    private const val CICLO_VIDA = "CICLOVIDA"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val botaoCadastrar = findViewById<Button>(R.id.btnCadastrar)
         val nomeUsuario = findViewById<EditText>(R.id.edtNomeUsuario)
-        val idadeUsuario= findViewById<EditText>(R.id.edtIdadeUsuario)
+        val idadeUsuario = findViewById<EditText>(R.id.edtIdadeUsuario)
 
         var contador = 0
 
-        botaoCadastrar.setOnClickListener{
+        botaoCadastrar.setOnClickListener {
             val nomeDigitado = nomeUsuario.text.toString()
             val idadeDigitada = idadeUsuario.text.toString()
 
-            if (nomeDigitado.isEmpty()){
-                if (idadeDigitada.isEmpty()){
+            if (nomeDigitado.isEmpty()) {
+                if (idadeDigitada.isEmpty()) {
                     idadeUsuario.error = "Por favor não esqueça de digitar sua idade!"
-                }else{
+                } else {
                     nomeUsuario.error = "Por favor digite seu nome."
                 }
-            } else{
+            } else {
                 val usuario = Usuario(contador++, nomeDigitado, idadeDigitada)
-                exibirUsuario(usuario)
+                exibirUsuario()
             }
         }
     }
 
-    fun exibirUsuario(usuario: Usuario){
-        Log.e("USUARIO", usuario.toString()) //pesquisar o erro, facilita na hora de encontrar no logcat
-        Toast.makeText(this,"Boas vindas, ${usuario.nome}, ${usuario.idade} anos! Seu id é ${usuario.id}",Toast.LENGTH_LONG).show()
+    fun exibirUsuario() {
+        Toast.makeText(
+            this, "Usuário cadastrado com sucesso!",
+            Toast.LENGTH_LONG
+        ).show()
+
+        redirecionar()
     }
 
+    fun redirecionar() {
+        val destinoActivity = Intent(this, UsuarioActivity::class.java)
+        startActivity(destinoActivity)
+        finish()
+    }
 
     override fun onStart() {
         super.onStart()
@@ -64,8 +73,15 @@ class MainActivity : AppCompatActivity() {
         Log.e(CICLO_VIDA, "App em OnRestart")
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Toast.makeText(
+            this, "vlw flw",
+            Toast.LENGTH_LONG).show()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.e(CICLO_VIDA, "App em OnStop")
     }
+}
